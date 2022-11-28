ec6c4b292df42c2cad48e89c8fc312c4539754c0
Suman Roy <95040233+Suman373@users.noreply.github.com>
2022-11-28 20:10:50 +0530

    add:banner items,sections

--- client/src/pages/public/Home/Home.jsx
+++ client/src/pages/public/Home/Home.jsx
@@ -2,22 +2,63 @@ import React from 'react';
 import './Home.scss';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import { AppBar, Typography } from '@mui/material';
 import NavbarHome from '../../../components/Navbar-home/NavbarHome';
+import { SiFsecure, SiWebmoney } from 'react-icons/si';
+import { FaHandshake } from 'react-icons/fa';
+import givetakeheart from '../../../assets/give-take-heart.webp';
 
 const Home = () => {
-  return (
-    <>
-        <Container>
-            <NavbarHome/>
-            <Box className="banner-wrapper" sx={{ bgcolor: '#ffffff', height: '100vh' }}>
-                <Typography className="banner-title" variant="h2">
-                    Hellooo
-                </Typography>
-            </Box>
-        </Container>
-    </>
-  )
+    return (
+        <>
+            <Container>
+                <NavbarHome />
+                <Box className="banner-wrapper" sx={{ bgcolor: '#ffffff', height: '100vh' }}>
+                    <section className="banner-slogan">
+                        <h1>
+                            Your Go-to Fundraising Platform
+                        </h1>
+                        <p>
+                            Join us to thrive towards helping millions of unpriviledged who need our help
+                        </p>
+
+                        <div>
+                            <button>
+                                Get started
+                            </button>
+                            <button>
+                                More from us
+                            </button>
+                        </div>
+                    </section>
+                    <section className="banner-illustration">
+                        <div className="icons-container">
+                            <i><SiFsecure /></i>
+                            <i><FaHandshake /></i>
+                            <i><SiWebmoney /></i>
+                            <i></i>
+                        </div>
+                        <div className="illustration-caption">
+                            <h1>
+                                Looking to help the needy but not sure of the authenticity?
+                            </h1>
+                            <p>
+                                Our site provides you that opportunity of making a huge difference to the lives of millions with small contribution. Yes, you might be worried about if it's safe. Don't worry, we have got you covered.
+                            </p>
+                        </div>
+                        <img src={givetakeheart} alt="give-take-heart" />
+                    </section>
+                </Box>
+                <Box className="platform-wrapper" sx={{ bgcolor: '#ffffff', height: '100vh' }}>
+                    <section className="our-platform">
+
+                    </section>
+                    <section className="platform-illustration">
+
+                    </section>
+                </Box>
+            </Container>
+        </>
+    )
 }
 
 export default Home;

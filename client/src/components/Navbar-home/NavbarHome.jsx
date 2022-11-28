ee618933449ed0b82500a0d9d5381f0078b61e44
Suman Roy <95040233+Suman373@users.noreply.github.com>
2022-11-28 13:29:17 +0530

    add:navbar-home
    fixed styling of navbar-home component

---
+++ client/src/components/Navbar-home/NavbarHome.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './NavbarHome.scss';
+
+const NavbarHome = () => {
+  return (
+    <>
+      <nav className='navbar-home'>
+          <a href="/">Helping-hand</a>
+          <ul>
+            <li><a href="#platform">Platform</a></li>
+            <li><a href="#pricing">Pricing</a></li>
+            <li><a href="#support">Support</a></li>
+            <li><a href="#blogs">Blogs</a></li>
+          </ul>
+          <div className="user-block">
+            <button>
+              Sign in
+            </button>
+          </div>
+      </nav>
+    </>
+  )
+}
+
+export default NavbarHome;
--- client/src/pages/public/Home/Home.jsx
+++ client/src/pages/public/Home/Home.jsx
@@ -3,12 +3,13 @@ import './Home.scss';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { AppBar, Typography } from '@mui/material';
+import NavbarHome from '../../../components/Navbar-home/NavbarHome';
 
 const Home = () => {
   return (
     <>
         <Container>
-            
+            <NavbarHome/>
             <Box className="banner-wrapper" sx={{ bgcolor: '#ffffff', height: '100vh' }}>
                 <Typography className="banner-title" variant="h2">
                     Hellooo

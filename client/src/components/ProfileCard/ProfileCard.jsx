51a5f9a3438515a582bd30617f5e2acfdfe669ca
Suman Roy <95040233+Suman373@users.noreply.github.com>
2023-07-04 17:47:47 +0530

    feat:dynamic profilecard w getuserapi

--- client/src/components/ProfileCard/ProfileCard.jsx
+++ client/src/components/ProfileCard/ProfileCard.jsx
@@ -1,53 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import './ProfileCard.scss';
 import defaultCover from '../../assets/blossom_fallback.jpg';
+import useAuth from '../../hooks/useAuth';
+import axios from 'axios';
 
 const ProfileCard = () => {
-    /** 
-const [profileDetails, setProfileDetails] = useState([]);
-const {_id} = useAuth();
- 
-// api to get user details
-const fetchProfileDetails = async()=>{
-  const data = await axios.get(`http://localhost:5000/users/${_id}`)
-  .catch((e)=>{
-    if(e.response){
-      console.log(e.response.data);
-    }else{
-      console.log(e.message);
+
+    const [profileDetails, setProfileDetails] = useState([]);
+    const { _id } = useAuth();
+
+    // unique user details
+    const fetchProfileDetails = async () => {
+        const data = await axios.get(`http://localhost:5000/user/${_id}`)
+            .catch((e) => {
+                if (e.response) {
+                    console.log(e.response.data);
+                } else {
+                    console.log(e.message);
+                }
+            });
+        alert(data?.data?.message);
+        console.log(data?.data?.result);
+        setProfileDetails(data?.data?.result);
     }
-  });
-  alert(data?.data?.message);
-  // setProfileDetails(data?.data);
-}
 
-useEffect(()=>{
-  fetchProfileDetails();
-},[]);
+    useEffect(() => {
+        fetchProfileDetails();
+    }, []);
 
-**/
+
     return (
         <div className="profile">
             <div className="cover">
                 <img className='cover-pic' src={defaultCover} alt="cover" />
-                <img className="profile-pic" src={defaultCover} alt="pfp" />
+                <img className="profile-pic" src={profileDetails?.profileImage? profileDetails.profileImage : defaultCover} alt="pfp" />
             </div>
             <div className="details">
-                <h3 className="name">Suman Roy</h3>
-                <p className="email">reachsuman.roy@gmail.com</p>
+                <h3 className="name">{profileDetails.name}</h3>
+                <p className="email">{profileDetails.email}</p>
                 <div className="count">
                     <div className="count-slots">
-                        Amount donated <span>$200</span>
+                        Amount donated <span>{profileDetails?.amountDonated}</span>
                     </div>
                     <div className="count-slots">
-                        Events held <span>1</span>
+                        Events held <span>{profileDetails?.eventsHeld.length}</span>
                     </div>
                     <div className="count-slots">
-                        Followers <span>15</span>
+                        Followers <span>{profileDetails?.followers.length}</span>
                     </div>
                     <div className="count-slots">
-                        Following <span>23</span>
-                    </div> 
+                        Following <span>{profileDetails?.following.length}</span>
+                    </div>
                 </div>
                 <button>
                     Show Profile

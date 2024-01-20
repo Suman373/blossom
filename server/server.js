0ec2fa4c69ab4f963bda8e5c417fa2dea315b404
Suman Roy <iamsuman898@gmail.com>
2024-01-20 14:31:43 +0530

    debugging db connection

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ const feed = require('./routes/feedRoute');
 const passportSetup = require('./config/passportSetup');
 const app = express();
 const PORT = process.env.PORT || 5000;
-mongoose.set('strictQuery',true);
 
 // middlewares
 app.use(cors({
@@ -46,7 +45,7 @@ app.get('/', (req,res)=>  {
 
 const connectToDB = async()=>{
     try {
-           await mongoose.connect(process.env.MONGO_DB_URI)
+           await mongoose.connect(process.env.MONGO_DB_URI, {useNewUrlParser:true});
            console.log(`MongoDB is connected successfully`);
         
     } catch (error) {

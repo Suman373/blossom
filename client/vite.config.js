09b47102da9dfb9cbe63950055f92fba9ab99be6
Suman Roy <iamsuman898@gmail.com>
2024-02-10 19:42:22 +0530

    feat:dockerized client dir

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server:{
-    open:true,
+    open:false,
+    host:true,
+    strictPort:true,
+    port:3000
   }
 });

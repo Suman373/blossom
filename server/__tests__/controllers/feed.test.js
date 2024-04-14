3b7bd8515f8aaa07bbe1aeefc55bd914e90a49e6
Suman Roy <iamsuman898@gmail.com>
2024-04-15 02:19:21 +0530

    feat:like/unlike feed,test:feed like unlike

--- server/__tests__/controllers/feed.test.js
+++ server/__tests__/controllers/feed.test.js
@@ -11,6 +11,7 @@ const req = {
     body: {
         userId:'507f1f77bcf86cd799439011',
         name:'fakename',
+        likeId:'507f1f77bcf86cd799439011'
     },
     params:{
         id:'507f1f77bcf86cd799439011'
@@ -107,3 +108,21 @@ test('Update user feed with id, 200', async()=>{
     expect(res.json).toHaveBeenCalledWith({message:"Feed updated successfully",result:mockFeed});
 });
 
+// like/unlike feed
+test('Like or unlike a feed, 200', async()=>{
+    await FeedModel.exists.mockImplementation((query)=> {
+        if(query && query._id==="507f1f77bcf86cd799439011") return true;
+        else return false;
+    })
+    await FeedModel.findByIdAndUpdate.mockResolvedValue((_id,update,options)=>{
+        if(_id && update && options) return {_id:_id, ...update};
+        else return null;
+    });
+    const res = mockRes();
+    await feedController.likeFeed(req,res);
+    // assert
+    expect(FeedModel.exists).toHaveBeenCalled();
+    expect(FeedModel.findByIdAndUpdate).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledTimes(1);
+});

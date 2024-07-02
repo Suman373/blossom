7ffd4dc8de051c45b8d3df2db43a94146544b8c4
Suman Roy <iamsuman898@gmail.com>
2024-07-02 21:11:39 +0530

    fix: pdf generation abs path

--- server/helpers/donationInvoice.js
+++ server/helpers/donationInvoice.js
@@ -1,5 +1,6 @@
 // ------ generate the invoice and send to user email
 
+const path = require("path");
 const sendMail = require("../services/email");
 const generatePDF = require("../services/generatePDF");
 const fs = require('fs');
@@ -8,14 +9,19 @@ const fs = require('fs');
 const donationInvoice = async(pdfData, templateType, cust_email) => {
     try {
         const pdfName = await generatePDF(pdfData, templateType);
-        if(!pdfName) throw new Error("PDF generation failed, could not send receipt");        
+        if(!pdfName) throw new Error("PDF generation failed, could not send invoice");
+        // abs path for invoice
+        const pdfPath = path.join(__dirname,'..','services','outputs',pdfName);
+        if (!fs.existsSync(pdfPath)) {
+            throw new Error(`PDF file ${pdfName} does not exist at path ${pdfPath}`);
+        }
         const emailPayload = {
             recipient: `${cust_email}`,
             subject: "Blossom: Thank you for your Generous Donation",
             text: "Thank you for your generous donation ❤️. Your support is greatly appreciated and will make a significant impact.\n\nPlease find your donation receipt attached to this email.Thank you once again for your kindness and support.\n\nBest regards, Blossom\n https://blossom-web-v1.vercel.app",
             attachments: [{
                 filename: `${pdfName}`,
-                path: `./services/outputs/${pdfName}`,
+                path: pdfPath,
                 contentType: 'application/pdf'
             }]
         }
@@ -24,7 +30,7 @@ const donationInvoice = async(pdfData, templateType, cust_email) => {
 
         // clear outputs dir
         setTimeout(() => {
-            fs.unlinkSync(`./services/outputs/${pdfName}`);
+            fs.unlinkSync(pdfPath);
         }, 8000);
         
     } catch (error) {
